fc6dd00a1d68d4359a647d45d85a303fed2d611a
Aadil-Nazir-Dar <daraadi4@gmail.com>
2022-09-29 15:04:21 +0530

    hide database password

--- source/database.py
+++ source/database.py
@@ -6,7 +6,7 @@ from source import app
 mysql=MySQL()
  
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '*****'
+app.config['MYSQL_DATABASE_PASSWORD'] = '****'
 app.config['MYSQL_DATABASE_DB'] = 'Application'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
